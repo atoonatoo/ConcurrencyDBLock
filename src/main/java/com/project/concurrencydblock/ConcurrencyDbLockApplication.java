a61fb1d6d8231274efd84e9e08616e8a4e3b24ba
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2026-03-16 19:19:48 +0900

    #1 init: 동시성 테스트 프로젝트 시작

---
+++ src/main/java/com/project/concurrencydblock/ConcurrencyDbLockApplication.java
@@ -0,0 +1,13 @@
+package com.project.concurrencydblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyDbLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyDbLockApplication.class, args);
+    }
+
+}
